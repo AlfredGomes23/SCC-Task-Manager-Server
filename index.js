bf5094615c1170ecbdf294be4e51be5352686e50
Alfred Gomes <alfredgomes23@users.noreply.github.com>
2023-12-22 19:24:26 +0600

    database connected

--- index.js
+++ index.js
@@ -3,6 +3,7 @@ const cors = require('cors');
 const port = process.env.PORT || 5000;
 require('dotenv').config();
 const jwt = require('jsonwebtoken');
+const { MongoClient, ServerApiVersion } = require('mongodb');
 const app = express();
 
 //middleware
@@ -10,6 +11,40 @@ app.use(express.json());
 app.use(cors({
     origin: ['http://localhost:5173']
 }));
+
+// Create a MongoClient with a MongoClientOptions object to set the Stable API version
+const client = new MongoClient(process.env.DB_URI, {
+    serverApi: {
+        version: ServerApiVersion.v1,
+        strict: true,
+        deprecationErrors: true,
+    }
+});
+
+async function run() {
+    try {
+        // Connect the client to the server	(optional starting in v4.7)
+        // await client.connect();
+        //database collections
+        const users = client.db("task-manager").collection("users");
+        //routes
+        app.get('/users', async (req, resp) => {
+            const result = await users.find().toArray();
+            resp.send(result);
+        });
+        // Send a ping to confirm a successful connection
+        // await client.db("admin").command({ ping: 1 });
+        // console.log("Pinged your deployment. You successfully connected to MongoDB!");
+    } finally {
+        // Ensures that the client will close when you finish/error
+        // await client.close();
+    }
+}
+run().catch(console.dir);
+
+
+
+
 //home route
 app.get('/', (req, resp) => {
     resp.send("Task Manager is Running.");
